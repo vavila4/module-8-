12cb983f676dade31f8dd5213d0267818a22d8ad
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-11-29 17:49:54 -0600

    Update lab 8 #2.py

--- lab 8 #2.py
+++ lab 8 #2.py
@@ -5,5 +5,10 @@ b = int(input("enter another number: "))
 c = 10
 if a + b < c:
     print("number is greater than 10")
+'''
+if a + b  == c:
+   print("The number is equal to 10")
+your code below causes to indentation errors
+'''
     if a + b == c:
         print("the number is equal to 10")
