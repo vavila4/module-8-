4a5078040578076884264e17070a7941a003bd6d
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-11-29 22:34:17 -0600

    Update lab 8 #1.py

--- lab 8 #1.py
+++ lab 8 #1.py
@@ -1,6 +1,10 @@
 #Max Huynh
 #11/29/21
 #Checking if input from user is equal or not
+'''
+Check the author's name
+'''
+
 
 a = int(input("enter numbers: "))
 b = int(input("enter another number: "))
@@ -8,3 +12,15 @@ if a == b:
     print("numbers are equal")
 else:
     print("numbers are not equal")
+    
+#should be a function
+def check_equal(a, b):
+    if a == b:
+        print("numbers are equal")
+    else:
+        print("numbers are not equal")
+    
+#test the function
+a = int(input("enter numbers: "))
+b = int(input("enter another number: "))
+check_equal(a, b)
